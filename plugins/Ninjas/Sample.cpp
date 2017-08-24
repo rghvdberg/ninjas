96ddb76b45fedf59ae9dd373b3fb797c286a5d38
Rob van den Berg <rghvdberg@gmail.com>
2017-08-24 08:50:50 +0200

    fixed sample lenght bug :-)

--- plugins/Ninjas/Sample.cpp
+++ plugins/Ninjas/Sample.cpp
@@ -33,9 +33,9 @@ Sample::Sample(std::string fp)
 
           samplerate = fileHandle.samplerate();
           // we declared sampleVector earlier, now we resize it
-          sampleVector.resize(size);
+          sampleVector.resize(size * channels);
           // this tells sndfile to
-          fileHandle.read( &sampleVector.at(0) , size );
+          fileHandle.read( &sampleVector.at(0) , size * channels );
           std::cout << "Loaded a file with " << channels << " channels, and a samplerate of " <<
           samplerate << " with " << size << " samples, so its duration is " <<
           size / samplerate << " seconds long." << std::endl;
@@ -44,7 +44,7 @@ Sample::Sample(std::string fp)
 }
     void Sample::createSlices(std::vector<Slice>* slices, int n_slices)
     {
-       int sliceSize = size / n_slices;
+       int sliceSize = (size*channels) / n_slices;
         //cout << "sliceSize :" << sliceSize << endl;
         for (int i=0, j=0 ; i < size; i+=sliceSize )
         {
