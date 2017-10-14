6a085428e8486a306ddfc739581db46a7f808e00
Rob van den Berg <rghvdberg@gmail.com>
2017-10-14 16:55:10 +0200

    artwork update

---
+++ plugins/Ninjas/Artwork/NinjasArtwork.hpp
@@ -0,0 +1,15 @@
+/* (Auto-generated binary data file). */
+
+#ifndef BINARY_NINJASARTWORK_HPP
+#define BINARY_NINJASARTWORK_HPP
+
+namespace NinjasArtwork
+{
+    extern const char* backgroundData;
+    const unsigned int backgroundDataSize = 1343628;
+    const unsigned int backgroundWidth    = 1276;
+    const unsigned int backgroundHeight   = 351;
+}
+
+#endif // BINARY_NINJASARTWORK_HPP
+
